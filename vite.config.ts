39b9c7a7e96f73b0f4865c3c2477540e7eac187d
michaelbguo <michaelbguo@tencent.com>
2026-01-12 20:22:03 +0800

    chore: update Vite configuration to increase chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
-
+  build: {
+    chunkSizeWarningLimit: 1024
+  },
   clearScreen: false,
   server: {
     port: 1420,
